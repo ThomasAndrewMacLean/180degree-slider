02ede883cbd99e655ad1a4fdb5f413c018384e03
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-01-31 13:19:13 +0100

    setup slider

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 180degree-slider
 
-
 https://djaytechdiary.com/setting-up-react-webpack-4-and-babel-7-from-scratch-2021
+
+https://dev.to/hankchizljaw/a-modern-css-reset-6p3
--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/index.css";
+import * as Styles from "./App.styles";
 const App = () => {
-  return <h1>Setting Up React,Webpack 4 & Babel 7!</h1>;
+  const [imageIndex, setImageIndex] = useState(1);
+  return (
+    <Styles.Main>
+      <Styles.Title>SLIDER</Styles.Title>
+
+      <img src={`./static/${imageIndex}.webp`} alt="" />
+      <Styles.Slider
+        type="range"
+        value={imageIndex}
+        onChange={(e) => setImageIndex(e.target.value)}
+        min="1"
+        max="36"
+        step="1"
+      />
+    </Styles.Main>
+  );
 };
 
 export default App;
