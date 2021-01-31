e3cf4ecce215236d44797e7d56f7abb19b65e354
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-01-31 13:46:08 +0100

    preload images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import "./styles/index.css";
 import * as Styles from "./App.styles";
 const App = () => {
   const [imageIndex, setImageIndex] = useState(1);
+
+  // load all images in memory
+  useEffect(() => {
+    for (let index = 1; index <= 36; index++) {
+      const image = new Image();
+      image.src = `./static/${index}.webp`;
+    }
+  }, []);
+
   return (
     <Styles.Main>
       <Styles.Title>SLIDER</Styles.Title>
