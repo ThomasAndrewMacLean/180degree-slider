879b52668c40391798f782e78a633d68d93b65d4
ThomasAndrewMacLean <hello@thomasmaclean.be>
2021-01-31 12:25:53 +0100

    setup modern react

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 180degree-slider
+
+
+https://djaytechdiary.com/setting-up-react-webpack-4-and-babel-7-from-scratch-2021
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles/index.css";
+const App = () => {
+  return <h1>Setting Up React,Webpack 4 & Babel 7!</h1>;
+};
+
+export default App;
